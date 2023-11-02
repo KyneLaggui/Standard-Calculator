acbf43842aef700c4fe7a0b7173cc94e68b57233
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-11-02 23:49:15 +0800

    Working Logic Calculator

--- script.js
+++ script.js
@@ -7,5 +7,43 @@
 //         document.calcu.screen.value = newValue;
 //     }
 // }
+const calcuScreen = document.querySelector(".calcu-screen");
 
+function valueNum(value) {
+    calcuScreen.value += value;
+    
+}
 
+function resetCalcu() {
+    calcuScreen.value = '';
+}
+
+function backspace() {
+    calcuScreen.value = calcuScreen.value.slice(0, -1);
+    
+}
+
+function performOperation(operator) {
+    var calcuScreen = document.getElementById("calcu-screen");
+    var screenValue = calcuScreen.value;
+    
+    if (screenValue !== '') {
+        var lastChar = screenValue.charAt(screenValue.length - 1);
+        if (lastChar !== '+' && lastChar !== '-' && lastChar !== '*' && lastChar !== '/') {
+            calcuScreen.value += operator;
+        }
+    }
+}
+
+    
+function calculateResult() {
+    var calcuScreen = document.getElementById("calcu-screen");
+    var screenValue = calcuScreen.value;
+    
+    try {
+        var result = eval(screenValue);
+        calcuScreen.value = result;
+    } catch (error) {
+        calcuScreen.value = 'Error';
+    }
+}
