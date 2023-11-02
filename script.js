54c16e0ec4558e98a794e2d2d385cb14ef53d31c
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-11-02 22:36:42 +0800

    Working Buttons on numerical backspace and reset

---
+++ script.js
@@ -0,0 +1,11 @@
+// function backspace() {
+//     var cvalScreen = document.calcu.screen.value;
+
+//     if (cvalScreen.length > 0) {
+//         var newValue = cvalScreen.slice(0, -1);
+
+//         document.calcu.screen.value = newValue;
+//     }
+// }
+
+
